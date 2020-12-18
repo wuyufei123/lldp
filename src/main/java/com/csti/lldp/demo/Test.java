4de19ff20b2a4a210739dc15f5f7c1cf7725cc6b
wuyufei123 <51846450+wuyufei123@users.noreply.github.com>
2020-12-18 09:59:53 +0800

    main文件读取写入csv

--- src/main/java/com/csti/lldp/demo/Test.java
+++ src/main/java/com/csti/lldp/demo/Test.java
@@ -1,15 +1,17 @@
 package com.csti.lldp.demo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import com.csvreader.CsvWriter;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     //解析txt转换为csv
     public static void main(String[] args) {
         //获取文件路径
-        String filePath ="D:\\cr\\222.92.224.1.txt";
+        String filePath = "D:\\cr\\222.92.224.1_12.16.txt";
         //调用
         readTxtFile(filePath);
     }
@@ -21,21 +23,27 @@ public class Test {
      * 3：读取到输入流后，需要读取生成字节流
      * 4：一行一行的输出。readline()。
      * 备注：需要考虑的是异常情况
+     *
      * @param filePath
      */
-    public static void readTxtFile(String filePath){
+    public static void readTxtFile(String filePath) {
         try {
-            String encoding="GBK";
-            File file=new File(filePath);
-            if(file.isFile() && file.exists()){ //判断文件是否存在
+            String encoding = "GBK";
+            File file = new File(filePath);
+            if (file.isFile() && file.exists()) { //判断文件是否存在
                 InputStreamReader read = new InputStreamReader(
-                        new FileInputStream(file),encoding);//考虑到编码格式
+                        new FileInputStream(file), encoding);//考虑到编码格式
                 BufferedReader bufferedReader = new BufferedReader(read);
                 String lineTxt = null;
-                while((lineTxt = bufferedReader.readLine()) != null){
+                //根据文件名获取ip
+                String ip = (file.getName().replace(".txt", "")).split("_")[0];
+                //日期
+                String data=(file.getName().replace(".txt", "")).split("_")[1];
+                List<String[]> lists = new ArrayList<>();
+                while ((lineTxt = bufferedReader.readLine()) != null) {
                     //截取
-                    String[] line=lineTxt.split("\\^");
-                    if(line.length!=0) {
+                    String[] line = lineTxt.split("\\^");
+                    if (line.length != 0) {
                         //获取端口名
                         String name = line[2];
                         //流量入
@@ -43,21 +51,38 @@ public class Test {
                         //流量出
                         String portOut = line[5];
                         //时间戳
-                        String timestamp=line[0].split(":")[1];
-                        System.out.println(timestamp);
+                        String timestamp = line[0].split(":")[1];
                         //写入文件（csv）
-
-
-
+                        String[] content = {ip, name, timestamp, portIn, portOut};
+                        lists.add(content);
                     }
                 }
+                writer(ip,data,lists);
                 read.close();
-            }else{
+            } else {
                 System.out.println("找不到指定的文件");
             }
         } catch (Exception e) {
             System.out.println("读取文件内容出错");
             e.printStackTrace();
         }
+    }
+
+    /*写入csv文件*/
+    public static void writer(String ip,String data,List<String[]> contents) throws IOException {
+        //当前日期
+
+        // 第一参数：新生成文件的路径 第二个参数：分隔符 第三个参数：字符集
+        CsvWriter csvWriter = new CsvWriter("D:\\cr\\csv\\"+ip+"_"+data+".csv", ',', Charset.forName("UTF-8"));
+        // 表头和内容
+        String[] headers = {"ip", "端口名", "时间戳", "流量入", "流量出"};
+        //String[] content = {"张三", "18", "男"};
+        // 写表头和内容，因为csv文件中区分没有那么明确，所以都使用同一函数
+        csvWriter.writeRecord(headers);
+        for (String[] content : contents) {
+            csvWriter.writeRecord(content);
+        }
+        // 关闭csvWriter
+        csvWriter.close();
     }
 }
